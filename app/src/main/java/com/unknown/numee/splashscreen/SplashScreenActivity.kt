3b35f00084caf5006b291326d672efa7e4f6b18b
Heghine Hakobyan <hakobyanheghine@gmail.com>
2018-09-02 22:18:09 +0400

    Added splash screen animation

--- app/src/main/java/com/unknown/numee/splashscreen/SplashScreenActivity.kt
+++ app/src/main/java/com/unknown/numee/splashscreen/SplashScreenActivity.kt
@@ -3,10 +3,14 @@ package com.unknown.numee.splashscreen
 import android.os.Bundle
 import android.os.Handler
 import android.support.v7.app.AppCompatActivity
+import android.widget.ImageView
+import com.bumptech.glide.load.engine.DiskCacheStrategy
+import com.bumptech.glide.request.RequestOptions
 import com.unknown.numee.MainActivity
 import com.unknown.numee.R
 import com.unknown.numee.login.LoginActivity
 import com.unknown.numee.registration.RegistrationActivity
+import com.unknown.numee.util.GlideApp
 import com.unknown.numee.util.Preferences
 
 class SplashScreenActivity : AppCompatActivity() {
@@ -23,6 +27,13 @@ class SplashScreenActivity : AppCompatActivity() {
         } else {
             startLoginActivity()
         }
+
+        val iconView = findViewById<ImageView>(R.id.splash_screen__icon)
+        GlideApp
+                .with(this).asGif()
+                .load(R.raw.logo_animation)
+                .apply(RequestOptions.diskCacheStrategyOf(DiskCacheStrategy.RESOURCE))
+                .into(iconView)
     }
 
     override fun onStop() {
@@ -34,21 +45,21 @@ class SplashScreenActivity : AppCompatActivity() {
         val handler = Handler()
         handler.postDelayed({
             MainActivity.startActivity(this@SplashScreenActivity)
-        }, 2000)
+        }, 6000)
     }
 
     private fun startLoginActivity() {
         val handler = Handler()
         handler.postDelayed({
             LoginActivity.startActivity(this@SplashScreenActivity)
-        }, 2000)
+        }, 6000)
     }
 
     private fun startRegistrationActivity() {
         val handler = Handler()
         handler.postDelayed({
             RegistrationActivity.startActivity(this@SplashScreenActivity)
-        }, 2000)
+        }, 6000)
     }
 
     private fun isUserLoggedIn(): Boolean {
