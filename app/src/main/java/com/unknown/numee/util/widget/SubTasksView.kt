9bf44f8454873c29569992ff2babcf65e562b62b
Heghine Hakobyan <hakobyanheghine@gmail.com>
2018-09-16 22:34:28 +0400

    Added subtasks view

---
+++ app/src/main/java/com/unknown/numee/util/widget/SubTasksView.kt
@@ -0,0 +1,17 @@
+package com.unknown.numee.util.widget
+
+import android.content.Context
+import android.util.AttributeSet
+import android.widget.FrameLayout
+
+
+class SubTasksView @JvmOverloads constructor(
+        context: Context,
+        attrs: AttributeSet? = null,
+        defStyleAttr: Int = 0
+) : FrameLayout(context, attrs, defStyleAttr) {
+
+    init {
+
+    }
+}
